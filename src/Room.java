b54786e9241ca82505f1e6ed1d28c39863a45e6c
Jonathan <Jonathan@Jonathan-PC>
2016-10-05 18:54:36 -0500

    Tile setup, room setup

--- src/Room.java
+++ src/Room.java
@@ -1,4 +1,16 @@
+import Tile;
+import java.util.ArrayList;
 
 public class Room {
+	private List<List<Tile>> floor;
+	//TODO walls
+	
+	public Room(){
+		this.floor = new ArrayList<List<Tile>>();
+	}
+	
+	public Room(String filename){
+		//TODO parse file
+	}
 	
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,4 +1,46 @@
-//
+
 public class Tile {
+	//handles carpet type, dirt level, and obstacle on the floor.
+	public final int CARPET_BARE = 0;
+	public final int CARPET_LOW = 1;
+	public final int CARPET_HIGH = 2;
+	
+	public final int OBSTACLE_NONE = 0;
+	public final int OBSTACLE_BLOCK = 1;
+	public final int OBSTACLE_STAIRS = 2;
+	
+	private int carpet;
+	private int dirtLevel;
+	private int obstacleType;
+	
+	public Tile(){
+		this.carpet=0;
+		this.dirtLevel=0;
+		this.obstacleType=0;
+	}
+	
+	public Tile(int carpetType, int dirt, int obstable){
+		this.carpet=carpetType;
+		this.dirtLevel=dirt;
+		this.obstacleType=obstacle;
+	}
+	
+	public void cleanTile(){
+		if (dirtLevel<=0) throw new Exception(); //TODO exception type
+		dirtLevel--;
+	}
+	
+	public int getCarpetType(){
+		return carpet;
+	}
+	
+	public int getDirtLevel(){
+		return dirtLevel;
+	}
+	
+	public int getObstacleType(){
+		return obstacleType;
+	}
+	
 	
 }
