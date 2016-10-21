0e2f808daafd1fe0915c7a892c3e198234d071c4
wengwenjun <rebecca1991123@gmail.com>
2016-10-20 23:09:53 -0500

    tiletest added

---
+++ src/test/TileTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cleansweep.Tile;
+
+public class TileTest {
+	Tile tile = new Tile();
+	@Test
+	public void isPlaceHolder() {
+		
+	   assertTrue(tile.isPlaceholder());
+		
+	}
+	@Test
+	public void testConstructor(){
+		int carpet = 1;
+		int dirt = 1;
+		int obstacleType = 1;	
+		Tile tile = new Tile(carpet, dirt, obstacleType); 
+		assertEquals(carpet,tile.getCarpetType());
+		assertTrue(tile.hasDirt());
+		tile.cleanTile();
+		assertFalse(tile.hasDirt());
+		assertEquals(obstacleType, tile.getObstacleType());
+	}
+	
+	
+
+}
